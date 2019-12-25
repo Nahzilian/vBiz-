f68f6121b0bbedc5af037583f056ee17b236e26f
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2019-12-25 13:36:38 -0500

    Created and added models 'companies'(migrated)

---
+++ vbiz/vbiz/companies/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=300)
+    b_address = models.CharField( max_length=200)
+    b_postal = models.CharField(max_length=50)
+    b_city = models.CharField(max_length=50)
+    c_phone = models.CharField(max_length=50)
+    c_email = models.EmailField(max_length=254)
+    service_type = models.CharField(max_length=100)
+    open_time = models.TimeField(auto_now=False, auto_now_add=False)
+    d_desc = models.CharField(max_length=50)
